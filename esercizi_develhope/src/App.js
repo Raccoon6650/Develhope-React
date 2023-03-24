7b9342a12b50008a74fb0a01a68a0964fe427c93
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-24 11:53:15 +0100

    Esercizio svolto

--- esercizi_develhope/src/App.js
+++ esercizi_develhope/src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello name="Mario"> </Hello>
+        <Hello> </Hello>
       </header>
     </div>
   );
 }
+
+//Il nome non viene inserito, succede solo questo
 
 export default App;

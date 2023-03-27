8ceafdec48846d26bff82a4a4e38d91e5f94deea
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-27 13:20:27 +0200

    Esercizio corretto

--- esercizi_develhope/src/App.js
+++ esercizi_develhope/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello name="Nino!"> </Hello>
-        <Hello name="Mario!"> </Hello>
-        <Hello name="Team!"> </Hello>
+        <Hello name="Nino!"/>
+        <Hello name="Mario!"/>
+        <Hello name="Team!"/>
       </header>
     </div>
   );

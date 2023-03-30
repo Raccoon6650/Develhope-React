a6c6ddb789f21e1238a5eda8d6dce8672345de26
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-30 12:23:31 +0200

    esercizio svolto

--- esercizi_develhope/src/components/welcome.js
+++ esercizi_develhope/src/components/welcome.js
@@ -1,20 +1,21 @@
-import Age from './age'
+import Age from "./age";
+
 const Welcome = (props) => {
-    return(
-        <>
-        <p>Welcome, {props.name}</p>
-        <Age age = {props.age}/>
-        </>
-    );
-}
+  return (
+    <>
+      <p>Welcome, {props.name}</p>
+      {props.age > 18 && <Age age={props.age} />}
+      {props.age > 18 ? <Age age={props.age} /> : null}
+    </>
+  );
+};
 
 Welcome.defaultProps = {
-    name: "You",
-    age: 0
-}
+  name: "You",
+  age: 0,
+};
 
 export default Welcome;
-
 
 /*import React, { Component } from 'react';
 

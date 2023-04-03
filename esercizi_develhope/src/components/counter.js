359e996cfb7fb19f7cb1607d265c2f619547faae
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-03 12:01:56 +0200

    Esercizio svolto

--- esercizi_develhope/src/components/counter.js
+++ esercizi_develhope/src/components/counter.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+/*import React, { useEffect, useState } from "react";
 import CounterDisplay from "./counter";
 
 const CounterT = function Counter(props) {
-  const [count, setCount] = useState(props.base);
-
+  CounterT.componentDidMount() {
+    const [count, setCount] = useState(props.base);
+  }
   useEffect(() => {
     const timer = setInterval(() => {
       setCount((prevCount) => prevCount + 1);
@@ -25,4 +26,37 @@ CounterT.defaultProps = {
 
 }
 
+export default CounterT;*/
+
+import React, { Component } from "react";
+import CounterDisplay from "./counter";
+
+class CounterT extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: props.base };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    }, this.props.time);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return <CounterDisplay count={this.state.count} />;
+  }
+}
+
+CounterT.defaultProps = {
+  base: 0,
+  time: 1000,
+};
+
 export default CounterT;
+
+//Il costruttore non è più necessario in questo caso
